47040ade55e5b54e14a89cba09ed809915b07114
Adrian Dinu <12354033+adrd@users.noreply.github.com>
2025-06-24 20:20:03 +0300

    Lesson 3 - useContext.

--- course/src/index.js
+++ course/src/index.js
@@ -9,8 +9,8 @@ import ReactDOM from "react-dom";
 */
 
 // import { App } from "./01-useState/App";
-import { App } from "./02-useReducer/App";
-//import {App} from './03-useContext/App'
+// import { App } from "./02-useReducer/App";
+import { App } from "./03-useContext/App";
 //import {App} from './04-useEffect/App'
 //import {App} from './05-encapsulate-state/App'
 //import {App} from './06-useNetwork/App'
